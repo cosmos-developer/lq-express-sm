92acdf77528cb0a285bcc77b20ad1b6d9381de67
Tuan Tran <tropicaldog17@gmail.com>
2023-11-16 22:37:34 +0700

    initial commit

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use lq_express_sm::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
